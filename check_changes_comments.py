c0c358ee1180be70cfbeded13b51ee04f693c3d1
Maxwell Li <liyuenan93@qq.com>
2023-07-14 10:46:41 +0800

    Update check_changes_comments.py

--- check_changes_comments.py
+++ check_changes_comments.py
@@ -6,8 +6,8 @@ from requests.auth import HTTPBasicAuth
 通过 Gerrit API 可以通过 Change ID 获取到该 Change 的所有 Comments，每个 Comment 会有 id、author、
 in_reply_to、updated、message、unresolved 等参数，第一个 Comment 没有 in_reply_to。每个 Comment 可能
 会有多条回复。下面针对某一处改动的 Comment 进行处理。
-获取该 Comment 下所有 Comment 的 id（unresolved==False 除外）和 in_reply_to，分别存入列表，两个列表相等，
-就可以证明该 Comment Resolved。
+获取该 Comment 下所有 Comment 的 id（unresolved==False 除外）和 in_reply_to，分别存入列表，comments_id
+列表为 reply_id 列表的子列表，就可以证明该 Comment Resolved。
 """
 
 Gerrit_USERNAME = "gerrit_username"
